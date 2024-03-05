d3019a42f28e706b7c13437be7c254a773f54092
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2024-03-05 10:00:00 +0100

    style: hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './Hero.css'
+import HeroImage from '../../assets/hero-image.png'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='hero'>
+        <div className='hero-contents'>
+            <h1>
+                <span className='buy'>Buy</span> 
+                <span className='from'>Anything From</span>
+                <span className='buy'>AMASLINK</span>
+            </h1>
+            <p className='description'>
+               your premier destination for cutting-edge technology and gadgets! Dive into a world of innovation <br />
+               where you can explore, select, and seamlessly purchase a wide array of gadgets to elevate your <br />
+               lifestyle
+            </p>
+        </div>
+        <div className='hero-image'>
+            <img src={HeroImage} alt="" />
+            </div>
+    </section>
   )
 }
 
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
+import Hero from '../../components/Hero/Hero'
 import Footer from '../../components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
 
 const Homepage = () => {
   return (
     <>
     <Header/>
+    <Hero/>
     <Footer/>
+    <Outlet/>
     </>
     
   )
